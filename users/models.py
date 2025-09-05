87ba212a2d6e67f2b9e80155db5e6361291a917c
awanat-olawale <olawaleawanat05@gmail.com>
2025-09-05 01:54:10 +0100

    feat: create users, wardrobe, planner and declutter apps

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    preferences = models.CharField(
+        max_length=100,
+        blank= True,
+        null = True
+    )
+    
+    def __str__(self):
+        return self.username
--- wardrobe/models.py
+++ wardrobe/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class WardrobeItem(models.Model):
+    CATEGORY_CHOICES = [
+        ("TOP", "Top"),
+        ("BOTTOM", "Bottom"),
+        ("HIJAB", "Hijab"),
+        ("SHOES", "Shoes"),
+        ("OTHER", "Other")
+    ]
+
+    SHOE_CHOICES = [
+        ("FLATS", "Flats"),
+        ("SANDALS", "Sandals"),
+        ("SNEAKERS", "Sneakers"),
+        ("BOOTS", "Boots"),
+        ('HEELS', "Heels")
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    subcategory = models.CharField(
+        max_length=50,
+        choices= SHOE_CHOICES,
+        blank= True,
+        null= True
+    )
+    colour = models.CharField(max_length=50)
+    fabric_type = models.CharField(max_length=100, blank=True, null =True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.CASCADE,
+        related_name= "wardrobe_items"
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+    
+    def clean(self):
+        #requires subcategory if category is SHOES
+        if self.category == "SHOES" and  not self.subcategory:
+            raise ValidationError("Subcategory is required if category is SHOES")
+        
+        #when category isn't SHOES, it prevents subcategory
+        if self.category != "SHOES" and self.subcategory:
+            raise ValidationError("Subcategory is only allowed when category is SHOES")
