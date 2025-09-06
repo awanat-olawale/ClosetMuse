4dc272d1b849821f0e07772c3ceb7dd942e88b12
awanat-olawale <olawaleawanat05@gmail.com>
2025-09-06 20:18:32 +0100

    feat: configure project-level URLS

--- closetmuse/urls.py
+++ closetmuse/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/', include('wardrobe.urls')),
+    path('api/', include('planner.urls')),
+    path('api/', include('declutter.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
