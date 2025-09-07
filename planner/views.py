122ecf91621e14e76d7c9759c6a6027541d90293
awanat-olawale <olawaleawanat05@gmail.com>
2025-09-07 20:57:17 +0100

    fix: add explicit 201 Created for all ViewSet POST endpoints

--- planner/views.py
+++ planner/views.py
@@ -22,3 +22,9 @@ class OutfitPlannerViewSet(viewsets.ModelViewSet):
         if item_ids:
             planner.items.set(item_ids)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
--- users/views.py
+++ users/views.py
@@ -13,4 +13,11 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action in ["create"]: 
             return [permissions.AllowAny()]
         return [permissions.IsAuthenticated()]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
