66c53ec8074d3831c095ca00dc4f2f0d3c3d540a
awanat-olawale <olawaleawanat05@gmail.com>
2025-09-07 06:03:20 +0100

    chore: prepare project for Render deployment

--- closetmuse/settings.py
+++ closetmuse/settings.py
@@ -28,9 +28,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
  
+if os.environ.get("RENDER"):
+    # Get the Render hostname without https://
+    RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+    if RENDER_EXTERNAL_HOSTNAME:
+        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
